00979a9c6b4146699f31472458403edfbcddc12c
phatba777-cmyk <phatba777@gmail.com>
2026-03-18 17:10:27 +0700

    bài 16

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("MSV:241505312228 - TenSV: Phat - Lop:225LTC01");
+
+        Console.Write("Nhap chuoi: ");
+        string s = Console.ReadLine();
+
+        Dictionary<char, int> count = new Dictionary<char, int>();
+
+        foreach (char c in s)
+        {
+            if (count.ContainsKey(c))
+                count[c]++;
+            else
+                count[c] = 1;
+        }
+
+        Console.WriteLine("\nSo lan xuat hien cua tung ky tu la:");
+        foreach (var x in count)
+            Console.WriteLine(x.Key + " : " + x.Value);
+
+        Console.ReadKey();
+    }
+}

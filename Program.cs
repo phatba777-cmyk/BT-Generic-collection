bba8537ebd34a4efc33c6989df19aa6a1776e0d6
phatba777-cmyk <phatba777@gmail.com>
2026-03-18 17:03:27 +0700

    bài 9

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("MSV:241505312228 - TenSV: Phat - Lop:225LTC01");
+
+        List<int> list = new List<int>();
+
+        Console.Write("Nhap so luong: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+            list.Add(int.Parse(Console.ReadLine()));
+
+        var kq = list.Distinct();
+
+        Console.WriteLine("Danh sach sau khi loai bo phan tu trung la:");
+        foreach (int x in kq)
+            Console.Write(x + " ");
+    }
+}

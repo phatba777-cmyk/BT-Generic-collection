699f35dd9617479fb3e712e52680473eb5405a85
phatba777-cmyk <phatba777@gmail.com>
2026-03-18 17:09:29 +0700

    bài 15

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("MSV:241505312228 - TenSV: Phat - Lop:225LTC01");
+
+        Dictionary<string, string> dict = new Dictionary<string, string>();
+
+        dict["hello"] = "xin chao";
+        dict["book"] = "sach";
+        dict["student"] = "sinh vien";
+
+        Console.Write("Nhap tu tieng Anh can tra: ");
+        string key = Console.ReadLine();
+
+        if (dict.ContainsKey(key))
+            Console.WriteLine("Nghia cua tu la: " + dict[key]);
+        else
+            Console.WriteLine("Khong tim thay tu trong tu dien.");
+
+        Console.ReadKey();
+    }
+}

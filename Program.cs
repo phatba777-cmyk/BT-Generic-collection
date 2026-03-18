06bb4158bac53709f56ae14db942440e3fef5e6b
phatba777-cmyk <phatba777@gmail.com>
2026-03-18 16:43:08 +0700

    Bài 2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("MSV:241505312228 - TenSV: Phat - Lop:225LTC01");
+
+        List<int> list = new List<int>();
+        int sum = 0;
+
+        Console.Write("Nhap n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+            list.Add(x);
+            sum += x;
+        }
+
+        Console.WriteLine("Tong = " + sum);
+    }
+}

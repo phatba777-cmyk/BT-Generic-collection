c3deb14fa756d171e6c3d1ca3d4de71943e07075
phatba777-cmyk <phatba777@gmail.com>
2026-03-18 16:47:15 +0700

    bài 3

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("MSV:241505312228 - TenSV: Phat - Lop:225LTC01");
+
+        List<int> list = new List<int>();
+
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+            list.Add(int.Parse(Console.ReadLine()));
+
+        int max = list[0];
+        foreach (int x in list)
+            if (x > max) max = x;
+
+        Console.WriteLine("Max = " + max);
+    }
+}

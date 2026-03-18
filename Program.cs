6f0333170a9434c21da39d54754eaefc4af9008d
phatba777-cmyk <phatba777@gmail.com>
2026-03-18 17:12:15 +0700

    bài 18

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Student
+{
+    public string Name;
+    public double Score;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("MSV:241505312228 - TenSV: Phat - Lop:225LTC01");
+
+        List<Student> list = new List<Student>();
+
+        Console.Write("Nhap so sinh vien: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"\nNhap sinh vien thu {i + 1}:");
+
+            Student s = new Student();
+
+            Console.Write("Nhap ten: ");
+            s.Name = Console.ReadLine();
+
+            Console.Write("Nhap diem: ");
+            s.Score = double.Parse(Console.ReadLine());
+
+            list.Add(s);
+        }
+
+        var max = list.OrderByDescending(x => x.Score).First();
+
+        Console.WriteLine("\nSinh vien co diem cao nhat la: " + max.Name);
+        Console.WriteLine("Diem: " + max.Score);
+
+        Console.ReadKey();
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("bai18_sinhviencodiemcaonhat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bai18_sinhviencodiemcaonhat")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88b1c294-88f1-4c46-9e3f-e1a9b361de2a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

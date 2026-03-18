abf9315cab8d9a8e12afea2a7b681c1a9c0cf460
phatba777-cmyk <phatba777@gmail.com>
2026-03-18 17:07:27 +0700

    bài 13

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Student
+{
+    public int Id;
+    public string Name;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("MSV:241505312228 - TenSV: Phat - Lop:225LTC01");
+
+        List<Student> list = new List<Student>();
+
+        Console.Write("Nhap so luong sinh vien: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"\nNhap sinh vien thu {i + 1}:");
+
+            Student s = new Student();
+
+            Console.Write("Nhap ID: ");
+            s.Id = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap ten: ");
+            s.Name = Console.ReadLine();
+
+            list.Add(s);
+        }
+
+        Console.WriteLine("\nTong so sinh vien trong danh sach la: " + list.Count);
+
+        Console.ReadKey();
+    }
+}
